4f0cfb52367047f28fec2eea610121d561edbd2c
Keliven Bordin Demarchi <70669566+iKeliven@users.noreply.github.com>
2024-07-01 14:46:31 -0300

    Delete src directory

--- src/assets/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import { Text } from "./Text"
-
-export default function Footer(){
-    return(
-        <div className="flex justify-center items-center p-5 w-[100vw] bg-white">
-            <Text size="small">One Ticket</Text>
-        </div>
-    )
-}
